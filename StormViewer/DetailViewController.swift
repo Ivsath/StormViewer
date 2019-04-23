4536ed9c03de4c4b1e2de71c851ad07673193147
Irvin Reyes <reyes.yerp@gmail.com>
2019-04-22 23:31:29 -0500

    Add sharing capabilities to the Detail view

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController {
         
         title = imageCountText
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -34,15 +35,10 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
-
+    
 }
